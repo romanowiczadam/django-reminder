9e3af328fcbd8685193a8f3bdea036ff1a28b222
Roman <instalacje14@gmail.com>
2020-08-17 17:11:50 +0200

    third commit

--- reminder/urls.py
+++ reminder/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reminderapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', views.signupuser, name='signupuser'),
+    path('current/', views.current_r, name='current_r'),
 ]
--- reminderapp/views.py
+++ reminderapp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 # Create your views here.
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'reminderapp/signupuser.html',
+                {'form' : UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('current_r')
+            except IntegrityError:
+                return render(request, 'reminderapp/signupuser.html',
+                              {'form': UserCreationForm(),
+                               'error': "This username already been taken"})
+        else:
+            return render(request, 'reminderapp/signupuser.html',
+                          {'form': UserCreationForm(),
+                           'error': "Passwords doesn't match to each other"})
+
+def current_r(request):
+    return render(request, 'reminderapp/current_r.html')
+
+
